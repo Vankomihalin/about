47136305f0822f67b079c8eca4f10b4ef95c94ef
Vankomihalin <133189093+Vankomihalin@users.noreply.github.com>
2023-05-25 09:13:34 +0400

    Add files via upload

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import avatar from "../../components/моя аватарка.png"
+</script>
+
+<h1>Михалин Иван</h1>
+<img src={avatar} alt="моя аватарка" height="250px" width="250px">
+<strong> <p>Мой возраст - 17 лет</p>
+<p>Хобби - спорт(биатлон)</p>
+<p>Моя страна - Россия</p>
+<p>Мой город - Ижевск</p>
+<p>Я ученик 56 гимназии</p>
+<p>Мой класс - 10А(физмат)</p></strong>
+<link rel="stylesheet" href="style.css">
+
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,10 @@
+<h1 class="privet">Мои контакты:</h1>
+    <strong><p>85642342345</p></strong>
+    <h1 class="privet">Моя почта:</h1>
+    <strong><p>mihalinivan23@gmail.com</p></strong>
+<style>
+    .privet { 
+        font-family: Arial, Helvetica, sans-serif; 
+        color: rgb(143, 8, 161); 
+    }
+</style>
